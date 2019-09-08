890283a8b5eabdd93e2a12f43e0ffab2ff73d22b
762124152 <762124152@qq.com>
2019-09-09 01:02:18 +0800

    首页已经全部渲染好[C

--- project/ziyi/js/home.js
+++ project/ziyi/js/home.js
@@ -2,12 +2,124 @@
     class HomeAjax{
         constructor(){
            
+           
             $.ajax({
-                url:"http://localhost/project/ziyi/data/home.json",
+                url:"http://localhost/project/ziyi/data/home2.json",
                 success:function(res){
-                    console.log(typeof res)
-                   
-
+                    var str="";
+                  for(var i in res){
+                      
+                    str+=`
+                        <p class="main-title">
+                            <span>+${res[i]["mian-title"]}+</span>
+                            <a href="#">${res[i]["mian-title"]}</a>
+                        </p>
+                        <div class="main-part">
+                            <div class="part-l">
+                                <a href="#">
+                                    <img src="${res[i]["goods-l"]}" />
+                                </a>
+                            </div>
+                            <div class="part-c">
+                                <a href="#">
+                                    <span>${res[i]["part-c1"]}</span>
+                                    <div class="part-r-price"> 
+                                        <i>￥${res[i]["c1-price"]}.00</i>
+                                        <b> ￥${res[i]["c1-noprice"]}.00</b>
+                                    </div>
+                                    <img class="part-c-img" src="${res[i]["goods-1"]}" />
+                                </a>
+                                <a href="#">
+                                    <span>${res[i]["part-c2"]}</span>
+                                    <div class="part-r-price">
+                                        <i>￥${res[i]["c2-price"]}.00</i>
+                                        <b> ￥${res[i]["c2-noprice"]}.00</b>
+                                    </div>
+                                    <img class="part-c-img" src="${res[i]["goods-2"]}"/>
+                                </a>
+                                <a href="#">
+                                    <span>${res[i]["part-c3"]}</span>
+                                    <div class="part-r-price">
+                                        <i>￥${res[i]["c3-price"]}.00</i>
+                                        <b> ￥${res[i]["c3-noprice"]}.00</b>
+                                    </div>
+                                    <img class="part-c-img" src="${res[i]["goods-3"]}" />
+                                </a>
+                                <a href="#">
+                                    <span>${res[i]["part-c4"]}</span>
+                                    <div class="part-r-price">
+                                        <i>￥${res[i]["c4-price"]}.90</i>
+                                        <b> ￥${res[i]["c4-noprice"]}.00</b>
+                                    </div>
+                                    <img class="part-c-img" src="${res[i]["goods-4"]}" />
+                                </a>
+                            </div>
+                            <!-- 最近热卖 最右边部分 -->
+                            <div class="part-r">
+                                <h6>最近热卖</h6>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <img src="${res[i]["part-r1"]}" />
+                                            <div class="part-r-tea">
+                                                <b>${res[i]["r1-title1"]}</b>
+                                                <div class="part-r-price">
+                                                    <i>￥${res[i]["r1-price"]}.00</i>
+                                                    <span>${res[i]["r1-comment"]}</span>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <img src="${res[i]["part-r2"]}" />
+                                            <div class="part-r-tea">
+                                                <b>${res[i]["r2-title1"]}</b>
+                                                <div class="part-r-price">
+                                                    <i>￥${res[i]["r2-price"]}.00</i>
+                                                    <span>${res[i]["r2-comment"]}</span>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <img src="${res[i]["part-r3"]}" />
+                                            <div class="part-r-tea">
+                                                <b>${res[i]["r3-title1"]}</b>
+                                                <div class="part-r-price">
+                                                    <i>￥${res[i]["r3-price"]}.00</i>
+                                                    <span>${res[i]["r3-comment"]}</span>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <img src="${res[i]["part-r4"]} "/>
+                                            <div class="part-r-tea">
+                                                <b>${res[i]["r4-title1"]}</b>
+                                                <div class="part-r-price">
+                                                    <i>￥${res[i]["r4-price"]}.00</i>
+                                                    <span>${res[i]["r4-comment"]}</span>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+                  }
+                  this.jsXuanRan=document.getElementById("jsXuanRan");
+                  this.jsXuanRan.innerHTML=str
                 }
             })
 
