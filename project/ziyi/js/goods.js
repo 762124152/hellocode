9d180f07c37a0e83971d2041853abc2a3876b696
762124152 <762124152@qq.com>
2019-09-10 21:12:16 +0800

    详情页面已经完成 就差购物车最后页面了

--- project/ziyi/js/goods.js
+++ project/ziyi/js/goods.js
@@ -2,7 +2,6 @@
     class Goods{
         constructor(){
             this.load();//ajax渲染函数
-           
         }
         //根据id值渲染页面
         load(){
@@ -10,7 +9,9 @@
                 url:"http://localhost/project/ziyi/data/list.json",
                 success(res){
                     this.id=localStorage.getItem("Id");
+                    
                     for(var i=0; i<res.length;i++){
+
                         this.n=this.id.slice(1,6);
                         if(res[i].goodsId==this.n){
                            this.str=`<a href="project/ziyi/home.html">首页</a>
@@ -20,7 +21,7 @@
                             <div class="goods-main">
                             <div class="main-l">
                                 <div class="loupe"></div>
-                                <div class="bigimg">
+                                <div class="bigObj">
                                     <img src="${res[i].goodsImg}" class="bigImg"/>
                                 </div>
                                     <img src="${res[i].goodsImg}" class="smallObj"/>
@@ -47,7 +48,7 @@
                                 </div>
                                 <div class="c-buy">
                                    <a href="#" class="nowBuy">立即购买</a>
-                                   <a href="#" class="shoppingCar">加入购物车</a>
+                                   <a  class="shoppingCar">加入购物车</a>
                                 </div>
                                 <p>100%正品 | 低价保障 | 7天无忧退货 | 专业营养师</p>
                             </div>
@@ -60,12 +61,15 @@
                          
                             this.jsXuanRan = document.getElementsByClassName("goods-title")[0];
                             this.jsXuanRan.innerHTML = this.str
+                            this.price=res[i].goodsPrice;
                         }
                       
                     }
+                    
                     this.loupeObj=document.getElementsByClassName("loupe")[0];
                     this.smallObj=document.getElementsByClassName("smallObj")[0];
-                    this.bigObj=document.getElementsByClassName("bigImg")[0];
+                    this.bigImg=document.getElementsByClassName("bigImg")[0];
+                    this.bigObj=document.getElementsByClassName("bigObj")[0];
                 
                     this.smallObj.onmousemove=(eve)=>{
                         eve=eve||window.event;
@@ -74,6 +78,7 @@
                         this.loupeObj.style.display="block";
                         this.loupeObj.style.left=this.mouseX-this.loupeObj.offsetWidth/2+"px";
                         this.loupeObj.style.top=this.mouseY-this.loupeObj.offsetHeight/2+"px";
+                        //判断放大镜不能出框
                         if(this.loupeObj.offsetTop<=0){ 
                             this.loupeObj.style.top=0;
                         }
@@ -86,24 +91,56 @@
                         if(this.loupeObj.offsetLeft>=(this.smallObj.offsetWidth-this.loupeObj.offsetWidth)) {
                             this.loupeObj.style.left=(this.smallObj.offsetWidth-this.loupeObj.offsetWidth)+"px"
                         };  
+
                         this.bigObj.style.display="block";
+                        this.bigImg.style.left=(-(this.loupeObj.offsetLeft/(this.smallObj.offsetWidth-this.loupeObj.offsetWidth))*(this.bigImg.offsetWidth-this.bigObj.offsetWidth))+"px";
+                        this.bigImg.style.top=(-(this.loupeObj.offsetTop/(this.smallObj.offsetHeight-this.loupeObj.offsetHeight))*(this.bigImg.offsetHeight-this.bigObj.offsetHeight))+"px";
+
+
+
                         this.loupeObj.onmouseleave=()=>{
                    
                             this.bigObj.style.display="none";
                             this.loupeObj.style.display="none";
                         }
-
-
+                        
+                       
                     }
                     
+                    this.btnObj=document.getElementsByName("num")[0];
+                    this.shopObj=document.getElementsByClassName("shoppingCar")[0];
+                    this.shopObj.onclick=(eve)=>{
+                        console.log(1);
+                        eve=eve|| window.event;
+                        eve.preventDefault()||eve.returnValue;
+                        this.car=localStorage.getItem("car")?JSON.parse(localStorage.getItem("users")) : [];
+                        if(this.car.length<1){
+                           this.car.push({
+                               id:this.id,
+                               price:this.price,
+                               num:this.btnObj.value
+                           })
+                       }else{
+                           this.state=ture;
+                           for(var i=0;i<this.car.length;i++){
+                               if(this.car[i].id==this.id){
+                                   this.car[i].num+this.btnObj.value;
+                                   state=false;
+                               }
+                           }
+                           if(state){
+                               this.car.push({
+                                    id:this.id,
+                                    price:this.price,
+                                    num:this.btnObj.value
+                               })
+                           }
+                       }
+                       localStorage.setItem("car",JSON.stringify(this.car));
+                    }
                 }
-
             })
-           
         }
-
-        
-
     }
     new Goods;
 })();
